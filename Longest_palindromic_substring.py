00be8731b7e54b3ddff826d534df5503eaf00447
Ashutosh Mishra <ashutoshm1771@gmail.com>
2019-09-03 22:05:27 +0545

    Update Longest_palindromic_substring.py

--- Longest_palindromic_substring.py
+++ Longest_palindromic_substring.py
@@ -1,8 +1,3 @@
-# A O(n^2) time and O(1) space program to find the  
-#longest palindromic substring 
-  
-# This function prints the longest palindrome substring (LPS) 
-# of str[]. It also returns the length of the longest palindrome 
 def longestPalSubstr(string): 
     maxLength = 1
   
@@ -14,7 +9,7 @@ def longestPalSubstr(string):
   
     # One by one consider every character as center point of  
     # even and length palindromes 
-    for i in xrange(1, length): 
+    for i in range(1, length): 
         # Find the longest even length palindrome with center 
     # points as i-1 and i. 
         low = i - 1
@@ -37,11 +32,11 @@ def longestPalSubstr(string):
             low -= 1
             high += 1
   
-    print "Longest palindrome substring is:", 
-    print string[start:start + maxLength] 
+    #print("Longest palindrome substring is:") 
+    print(string[start:start + maxLength]) 
   
-    return maxLength 
+    return 
   
 # Driver program to test above functions 
-string = "forgeeksskeegfor"
-print "Length is: " + str(longestPalSubstr(string)) 
+string =input()
+longestPalSubstr(string)
