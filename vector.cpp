7325be5deac2a606503053c888e29e06821643c3
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-19 22:46:55 +0530

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+int main()
+{
+	set<int> s;
+	s.insert(3);
+	s.insert(2);
+	for(int i=0;i<s.size();i++)
+	{}	
+	vector<int> v;
+	v.push_back(7);
+	cout<<v[0]<<endl;
+	cout<<"Hello";
+	return 0;
+	
+}
