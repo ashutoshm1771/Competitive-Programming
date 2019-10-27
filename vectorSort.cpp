114dc3bb5aed866bf47b022e441289aa8052d7cc
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-27 21:05:07 +0530

    Create vectorSort.cpp

---
+++ vectorSort.cpp
@@ -0,0 +1,14 @@
+//#include 'stdafx.h'
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int n;
+	vector<int> v ={5,4,3,2,1};
+	//cout<<v.begin()<<endl;
+	sort(v.begin(),v.end());
+	for(int i=0;i<v.end();i++)
+	cout<<v[i]<<" ";
+}
