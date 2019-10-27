6eb3e253916dde344977b2fd1ea28def01e13879
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-27 21:02:32 +0530

    Create vectorSortDescending.cpp

---
+++ vectorSortDescending.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool f(int x,int y)
+{
+	return x>y;
+}
+int main()
+{
+	int n,ip;
+	cin>>n;
+	vector<int>v;
+	while(n--)
+	{
+		cin>>ip;
+		v.push_back(ip);
+	}
+	sort(v.begin(),v.end(),f);
+	for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<<" ";
+	}
+}
