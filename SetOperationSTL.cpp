e7da91b5d1ae3b2c2ceade0cfeb2650176b56a7a
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-10-28 22:50:05 +0530

    Create SetOperationSTL.cpp

---
+++ SetOperationSTL.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    set<int>s;
+    int a[2];
+    while(n--)
+    {
+        cin>>a[0]>>a[1];
+        if(a[0]==1)
+        {
+            s.insert(a[1]);
+        }
+        else if(a[0]==2)
+        {
+            s.erase(a[1]);
+        }
+        else
+        {
+            set<int>::iterator it=s.find(a[1]);
+            if(it!=s.end())
+            {
+                cout<<"Yes"<<endl;
+            }
+            else
+            {
+                cout<<"No"<<endl;
+            }
+        }
+    }   
+    return 0;
+}
+
+
+
+
