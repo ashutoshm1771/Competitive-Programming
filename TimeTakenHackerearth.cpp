cb09b48736b286a3d99c5cc2930cdf87b0364208
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-11-22 13:36:25 +0530

    Create TimeTakenHackerearth.cpp

---
+++ TimeTakenHackerearth.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,r,ip;
+        cin>>n>>r;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        {
+            cin>>ip;
+            v.push_back(ip);
+        }
+        sort(v.begin(),v.end());
+        if(r-v[0]<0)
+        cout<<"0"<<endl;
+        else
+        cout<<r-v[0]<<endl;
+    }
+}
