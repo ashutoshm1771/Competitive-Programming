28feea86cbcabca2c86e52de21ea40d515ad5ea5
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-11-26 22:06:24 +0530

    Create CaesarCipherProblem.cpp

---
+++ CaesarCipherProblem.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isAlpha(char c)
+{
+    if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+        return true;
+    else
+    return false;
+}
+bool isLower(char c)
+{
+    if(c>='a' && c<='z')
+    return true;
+    return false;
+}
+
+string caesarCipher(string s, int k) {
+    
+    for(int i=0;s[i];i++)
+    {
+        if(isAlpha(s[i]))
+        {
+            if(isLower(s[i]))
+                s[i]=(s[i]+k-97)%26+97;
+            else
+                s[i]=(s[i]+k-65)%26+65;
+        }
+    }
+    return s;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result = caesarCipher(s, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
