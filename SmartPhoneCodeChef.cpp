aeaa45fa72841ab6c4549a30c44cfecc0b3f45fb
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-11-29 18:55:09 +0530

    Create SmartPhoneCodeChef.cpp

---
+++ SmartPhoneCodeChef.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+
+int main() {
+	IOS;
+	ll n,r,maxim=0;
+	cin>>n;
+	ll budgets[n];
+	for(int i=0;i<n;i++)
+	    cin>>budgets[i];
+	int size=sizeof(budgets)/sizeof(budgets[0]);
+	sort(budgets,budgets+n);
+	for(int i=0;i<n;i++)
+	{
+	    r=budgets[i]*(n-i);
+	    maxim=max(r,maxim);
+	}
+	cout<<maxim;
+	return 0;
+}
+
