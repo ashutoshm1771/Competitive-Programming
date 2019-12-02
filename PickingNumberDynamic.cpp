294b8a9d11052944a8cc019b251fd5ce2506ebc8
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-02 23:43:37 +0530

    Hackerrank

---
+++ PickingNumberDynamic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int n,a[100],b[200];
+int main() {
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int k= 2*i;
+            if(a[i]==a[j]||a[i]==a[j]+1)
+                b[k]++;
+            if(a[i]==a[j]||a[i]==a[j]-1)
+                b[k+1]++;
+        }
+    }
+    for(int i=0;i<200;i++)
+        if(b[i]>b[0])
+            b[0]=b[i];
+    cout<<b[0];
+    return 0;
+}
