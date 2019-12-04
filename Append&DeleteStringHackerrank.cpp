da9b97a964dd98463cad7133c6a41c18c6293ee9
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-04 23:47:11 +0530

    Create Append&DeleteStringHackerrank.cpp

---
+++ Append&DeleteStringHackerrank.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define L (s.size() + t.size())
+
+int main(){
+    string s, t;
+    int k, i;
+    cin >> s >> t >> k;
+    for(i = 0; s[i] == t[i]; i++);
+    cout << (L <= k + i*2 && L%2 == k%2 || L < k ? "Yes" : "No");
+    return 0;
+}
