c6091f6e274325de82bc8e1b8804e72fbe8e4c0f
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-05 09:24:18 +0530

    Create StrangeCounterHackerrank.cpp

---
+++ StrangeCounterHackerrank.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the strangeCounter function below.
+long strangeCounter(long t) {
+    long total=3,n=3;
+    while(t>total)
+    {
+        n=n*2;
+        total+=n;
+    }
+    long time=1;
+    while(t!=total)
+    {
+        total--;
+        time++;
+    }
+    return time;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    long t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
