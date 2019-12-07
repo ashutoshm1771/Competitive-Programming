10e0dd8f37f0cdaa10b986110ba0673a595c8bde
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-07 21:15:37 +0530

    Create SamandSubstringHackerrank.cpp

---
+++ SamandSubstringHackerrank.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+// Complete the substrings function below.
+int substrings(string n) {
+    long long int f=1;
+    int res=0;
+    for(int i=n.size()-1;i>=0;i--)
+    {
+        res=(res+(n[i]-'0')*f*(i+1))%MOD;
+        f=(f*10+1) % MOD;
+    }
+    return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n;
+    getline(cin, n);
+
+    int result = substrings(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
