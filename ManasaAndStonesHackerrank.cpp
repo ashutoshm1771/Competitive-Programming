ba967b671b11bca06b86645876a84d1d3488a30a
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-08 10:53:32 +0530

    Create ManasaAndStonesHackerrank.cpp

---
+++ ManasaAndStonesHackerrank.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the stones function below.
+vector<int> stones(int n, int a, int b) {
+    set<int> s;
+    
+    for(int i=0;i<n;i++)
+    {
+        s.insert(a*i+b*(n-i-1));
+    }
+    vector<int> v(s.begin(),s.end());
+    return v;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int T;
+    cin >> T;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int T_itr = 0; T_itr < T; T_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int a;
+        cin >> a;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int b;
+        cin >> b;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        vector<int> result = stones(n, a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
