e20f90dfe5344f8f08b65d5c5272789cc746e1c0
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-09 22:33:39 +0530

    Create ClimbingTheLeaderBoard2.cpp

---
+++ ClimbingTheLeaderBoard2.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio; cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int main()
+{
+    IOS
+    int n,ip,j;
+    vector<int> v,s,res;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>ip;
+        v.push_back(ip);
+    }
+    int ns;
+    cin>>ns;
+    for(int i=0;i<ns;i++)
+    {
+        cin>>ip;
+        s.push_back(ip);
+    }
+    map<int,int> m;
+    m[v[0]]=1;
+    for(int i=1;i<n;i++)
+    if(v[i-1]==v[i]) m[v[i]]=m[v[i-1]];
+    else m[v[i]]=m[v[i-1]]+1;
+    j=n-1;
+    for(int i=0;i<ns;i++)
+    {
+        while(v[j]<s[i] && j>=0) j--;
+        if(v[j]==s[i]) res.push_back(m[v[j]]);
+        else res.push_back(m[v[j]]+1);
+    }
+    for(int x: res) cout<<x<<endl;
+}
+
