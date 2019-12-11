78bef299333ad9140678ca94d60504eead0c9ee4
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-11 21:24:09 +0530

    Create MaxHourGlassHackerrank.cpp

---
+++ MaxHourGlassHackerrank.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    int sum=-INT_MAX;
+    for(int i=0;i<arr.size()-2;i++)
+    {
+        for(int j=0;j<arr[0].size()-2;j++)
+            sum=max(sum,(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]));
+    }
+    return sum;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
