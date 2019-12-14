13adbe76ee82ffd0992530b4ea76001a32fc7954
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-14 20:39:21 +0530

    Create TheatresSquaresCodeForces.cpp

---
+++ CodeForces/TheatresSquaresCodeForces.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	long double n,m,a;
+	cin>>n>>m>>a;
+	long double r=ceil(n/a);
+	long double c=ceil(m/a);
+	if(r==1 && c==1)cout<<1;
+	else if(r==1 && c>1)cout<<c;
+	else if(r>1 && c==1)cout<<r;
+	else cout<<(long long int)(r*c);
+	return 0;
+}
