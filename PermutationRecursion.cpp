2e4014ca7486ed930c9bba7a7d4f80c705ff2903
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-15 13:34:12 +0530

    Create PermutationRecursion.cpp

---
+++ PermutationRecursion.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void permute(vector<int> &v,int l,int r)
+{
+	if(l==r)
+	{
+		for(int i=0;i<v.size();i++)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	}
+	else
+	{
+		for(int i=l;i<=r;i++)
+		{
+			swap(v[l],v[i]);
+			permute(v,l+1,r);
+			swap(v[l],v[i]);
+		}
+	}
+}
+int main()
+{
+	int n,ip;
+	cin>>n;
+	vector<int> v;
+	for(int i=0;i<n;i++)
+	{
+		cin>>ip;
+		v.push_back(ip);
+	}
+	permute(v,0,n-1);
+}
