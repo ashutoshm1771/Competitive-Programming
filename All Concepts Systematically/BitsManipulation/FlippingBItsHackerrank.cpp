be9ccb01af9496a0d3dd566c30060b3af2245cb1
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-15 14:47:37 +0530

    Create FlippingBItsHackerrank.cpp

---
+++ All Concepts Systematically/BitsManipulation/FlippingBItsHackerrank.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the flippingBits function below.
+long flippingBits(long n) {
+    bitset<32> bset(n);
+    for(int i=0;i<bset.size();i++)
+    {
+        bset.flip(i);
+    }
+    return bset.to_ulong();
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
