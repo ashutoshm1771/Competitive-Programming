2bccbda879e779685d6c9f4d7ad83fb666efca50
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-16 21:14:55 +0530

    Create LuckBalanceHackerrank.cpp

---
+++ LuckBalanceHackerrank.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+bool f(int x,int y)
+{
+    return x>y;
+}
+// Complete the marcsCakewalk function below.
+long marcsCakewalk(vector<int> calorie) {
+    long c=0;
+    sort(calorie.begin(),calorie.end(),f);
+    for(int i=0;i<calorie.size();i++)
+    {
+        c+=(calorie[i]*pow(2,i));
+    }
+    return c;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string calorie_temp_temp;
+    getline(cin, calorie_temp_temp);
+
+    vector<string> calorie_temp = split_string(calorie_temp_temp);
+
+    vector<int> calorie(n);
+
+    for (int i = 0; i < n; i++) {
+        int calorie_item = stoi(calorie_temp[i]);
+
+        calorie[i] = calorie_item;
+    }
+
+    long result = marcsCakewalk(calorie);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
