0c687164f5218da91c384c5bc973e59a551c4635
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-17 22:18:48 +0530

    Create CandiesGreedyHackerrank.cpp

---
+++ CandiesGreedyHackerrank.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> r(n); 
+    vector<int> c(n); 
+    
+    int numcandies=1; 
+    c[0]=1;
+    cin >> r[0];
+    for (int i=1;i<n;i++) {
+        cin >> r[i];
+        if (r[i]>r[i-1]) numcandies++;
+        else if (r[i]<=r[i-1]) numcandies=1;
+        c[i]=numcandies;
+    }
+
+    numcandies=1;
+    long total=c[n-1];
+    for (int i=n-2;i>=0;i--) {
+        if (r[i]>r[i+1]) numcandies++;
+        else if (r[i]<=r[i+1]) numcandies=1;
+        c[i]=(numcandies>c[i]?numcandies:c[i]); 
+        total+=c[i];
+    }    
+    
+    cout << total;
+    return 0;
+}
