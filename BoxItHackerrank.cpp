7dbe0fd87a5306328feea872770db4a84b8e58e2
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-21 18:41:31 +0530

    Create BoxItHackerrank.cpp

---
+++ BoxItHackerrank.cpp
@@ -0,0 +1,26 @@
+
+
+// Write your Student class here
+class Student
+{
+    public:
+    int scores[5];
+    void input(){
+        {
+            for(int i=0;i<5;i++)
+            {
+                cin>>scores[i];
+            }
+        }
+    }
+    int calculateTotalScore()
+    {
+        int s;
+        for(int i=0;i<5;i++)
+        {
+            s+=scores[i];
+        }
+        return s;
+    }
+};
+
