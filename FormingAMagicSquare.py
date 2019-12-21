3b60247a04536d9fc9839263bbf06508ef5e2f13
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-21 18:48:08 +0530

    Create FormingAMagicSquare.py

---
+++ FormingAMagicSquare.py
@@ -0,0 +1,26 @@
+import sys
+diffs = []
+s = []
+for s_i in range(3):
+   s_t = [int(s_temp) for s_temp in input().strip().split(' ')]
+   s.append(s_t)
+
+all_possible = [
+            [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+            [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+            [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+            [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+            [[2, 7, 6], [9, 5, 1], [4, 3, 8]],]
+
+#compare s to each in all possible get number of differences for each to diffs
+for possiblity in all_possible:
+    cost = 0
+    for p_row, s_row in list(zip(possiblity,s)):
+        for p_num, s_num in (list(zip(p_row, s_row))):
+            if p_num != s_num:
+                cost += abs(p_num - s_num)
+    diffs.append(cost)
+print(min(diffs))
