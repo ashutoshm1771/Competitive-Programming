7a57a53236b2e2ed0c0e7286417584828892a88a
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-23 20:35:40 +0530

    Create BitwiseOperators.c

---
+++ BitwiseOperators.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  int i,j,ma=0,mo=0,mx=0,a,b,c;
+  for(i=1;i<n;i++)
+  {
+      for(j=i+1;j<=n;j++)
+      {
+            a=i&j;
+            if(a>ma && a<k)ma=a;
+            b=i|j;
+            if(b>mo && b<k)mo=b;
+            c=i^j;
+            if(c>mx && c<k)mx=c;
+      }
+  }
+  printf("%d\n%d\n%d",ma,mo,mx);
+}
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
+
