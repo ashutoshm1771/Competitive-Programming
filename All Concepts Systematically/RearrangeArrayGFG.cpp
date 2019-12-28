e015a72f2d3cec32f11647aa1b397fbcc9579f81
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-28 21:33:03 +0530

    Create RearrangeArrayGFG.cpp

---
+++ All Concepts Systematically/RearrangeArrayGFG.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,ip;
+	    cin>>n;
+	    vector<int> v;
+	    bool flag=true;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>ip;
+	        v.push_back(ip);
+	    }
+	    int i=0,j=n-1;
+	    while(i<=j)
+	    {
+	        if(flag)
+	        {
+	            cout<<v[j]<<" ";
+	            j--;
+	            flag=false;
+	        }
+	        else
+	        {
+	            cout<<v[i]<<" ";
+	            i++;
+	            flag=true;
+	        }
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
