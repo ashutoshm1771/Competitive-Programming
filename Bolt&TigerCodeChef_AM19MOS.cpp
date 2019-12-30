74a25cb425c2159964f84d4a74e7ff5674e96513
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-30 21:57:49 +0530

    extra

---
+++ Bolt&TigerCodeChef_AM19MOS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+typedef long int li;
+typedef long double ld;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	li t;
+	cin>>t;
+	while(t--)
+	{
+	    ld f,d,ta,bs;
+	    cin>>f>>d>>ta>>bs;
+	    ld tb=f/bs;
+	    ld tt=sqrt( (2*(f+d)) / ta );
+	    if(tb<tt)cout<<"Bolt"<<endl;
+	    else cout<<"Tiger"<<endl;
+	}
+	return 0;
+}
+
