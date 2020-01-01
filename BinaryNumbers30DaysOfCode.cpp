632aaf8a78cb9bd581bddc48f906753f62132414
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-01-01 11:53:18 +0530

    Create BinaryNumbers30DaysOfCode.cpp

---
+++ BinaryNumbers30DaysOfCode.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n,c=0,cp=0;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    bitset<64> bset(n);
+    bool flag=true;
+    for(int i=0;i<bset.size();)
+    {
+        c=0;
+        while(bset[i]==1){
+            c++;
+            i++;
+        }
+        cp=max(c,cp);
+        i++;
+        
+    }
+    cout<<cp;
+    return 0;
+}
+
