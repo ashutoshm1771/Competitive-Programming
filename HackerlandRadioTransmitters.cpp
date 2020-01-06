56dc7ac665ee8b7b7dd3cd350b8a28d47f742c90
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-01-06 19:18:37 +0530

    Create HackerlandRadioTransmitters.cpp

---
+++ HackerlandRadioTransmitters.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+     int i,j,c=0,x,k,n;
+     cin>>n>>k;
+     int ar[n];
+     for(i=0;i<n;i++)
+     cin>>ar[i];
+     sort(ar,ar+n);
+     i=0;
+     while(i<n)
+     {
+         j=i+1;
+         while((ar[i]+k>=ar[j])&&(j<n))
+         {
+             j+=1;
+         }
+         x=j;
+         j-=1;
+         while(ar[j]+k>=ar[x]&&(x<n))
+         {
+             x++;
+         }
+         c++;
+         i=x;
+     }
+    cout<<c;
+    return 0;
+}
