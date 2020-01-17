ebde8983749867c7b848bdc8cf68d0aef6da0a9e
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-01-17 15:17:44 +0530

    Create Sort0s1sAnd2s.cpp

---
+++ Sort0s1sAnd2s.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int ip,n;
+	    vector<int> v;
+	    cin>>n;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>ip;
+	        v.push_back(ip);
+	    }
+	    int low=0,mid=0,high=n-1;
+	    while(mid<=high)
+	    {
+	        if(v[mid]==0)
+	        {
+	            swap(v[low],v[mid]);
+	            low++;
+	            mid++;
+	        }
+	        else if(v[mid]==2)
+	        {
+	            swap(v[mid],v[high]);
+	            high--;
+	        }
+	        else
+	            mid++;
+	    }
+	    for(int i=0;i<n;i++)
+	    cout<<v[i]<<" ";
+	    cout<<endl;
+	}
+	return 0;
+}
