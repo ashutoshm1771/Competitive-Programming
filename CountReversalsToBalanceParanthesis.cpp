2683c8221a900d6056eb90de3135be87055676ed
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-03-02 15:12:10 +0530

    Create CountReversalsToBalanceParanthesis.cpp

---
+++ CountReversalsToBalanceParanthesis.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	cout<<s.size()<<endl;
+	if(s.size()%2){	
+		cout<<-1<<endl;
+		exit(0);
+	}
+	stack<int> st;
+	for(int i=0;i<s.size();i++)
+	{
+		if(s[i]=='}' && st.size())
+		{
+			if(st.top()=='{') st.pop();
+			else st.push(s[i]);
+		}
+		else st.push(s[i]);
+	}
+	int rl=st.size();
+	int n=0;
+	while(!st.empty())
+	{
+		if(st.top()=='{')n++;
+		st.pop();
+	}
+	cout<<(rl/2+n%2)<<endl;
+	return 0;
+}
