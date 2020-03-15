f44ae919e94251f808f7ddf64d7a63e8f41d9b64
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-03-15 15:54:22 +0530

    Create Replicate_Answer_Python.py

---
+++ Spurthi20_Codex_Final_QnA/1st And 2nd Years/Replicate/Replicate_Answer_Python.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    d=dict()
+    for i in l:
+        d[i]=0
+    for i in l:
+        d[i]+=1
+    m=0
+    for i in d.keys():
+        if(m<d[i]):
+            m=d[i]
+    print(n-m)
