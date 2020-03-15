6cae3f55233d46e049dffe95d7a8719910d46026
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-03-15 15:58:39 +0530

    Create String_Compression_Answer_C++.cpp

---
+++ Spurthi20_Codex_Final_QnA/1st And 2nd Years/String Compression/String_Compression_Answer_C++.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() { 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int c=0;
+        string s;
+        cin>>s;
+        for(int i=0;i<s.size();i++)
+            if(s[i]!=s[i+1]) c++;
+        cout<<2*c<<endl;
+    }
+    return 0;
+}
