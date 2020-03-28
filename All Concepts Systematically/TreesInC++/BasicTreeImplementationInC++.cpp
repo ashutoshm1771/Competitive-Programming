433eae6e57643cd0ffc8c8273f5d48f4738d2280
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-03-28 09:54:54 +0530

    trees

---
+++ All Concepts Systematically/TreesInC++/BasicTreeImplementationInC++.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+struct Node{
+	int data;
+	struct Node *left,*right;
+	Node(int data){
+		this->data=data;
+		left=right=NULL;
+	}
+};
+typedef struct Node* tptr;
+
+void inorder(tptr root)
+{
+	if(root==NULL)return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+void preorder(tptr root)
+{
+	if(root==NULL)return;
+	cout<<root->data<<" ";
+	preorder(root->left);
+	preorder(root->right);
+}
+void postorder(tptr root)
+{
+	if(root==NULL)return;
+	postorder(root->left);
+	postorder(root->right);
+	cout<<root->data<<" ";
+}
+int main()
+{
+	tptr root=new Node(5);
+	root->left=new Node(6);
+	root->right=new Node(7);
+	inorder(root); cout<<endl;
+	preorder(root); puts("");
+	postorder(root); puts("");
+	return 0;
+}
