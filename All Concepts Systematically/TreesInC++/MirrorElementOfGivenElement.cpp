1599ca5fd2e0c10e1ca088d08a0f39b7261459eb
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-03-28 20:42:19 +0530

    .

---
+++ All Concepts Systematically/TreesInC++/MirrorElementOfGivenElement.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+struct Node{
+	int data;
+	struct Node *left,*right;
+	Node(int data)
+	{
+		this->data=data;
+		left=right=NULL;
+	}
+};
+typedef struct Node* tptr;
+tptr bstInsert(tptr root,int data)
+{
+	if(root==NULL)return new Node(data);
+	if( data < root->data ) root->left=bstInsert(root->left,data);
+	else if( data > root->data ) root->right=bstInsert(root->right,data);
+	else return root;
+}
+int findMirror(int target,tptr left,tptr right)
+{
+	if(left==NULL || right==NULL)return 0;
+	if(left->data==target)return right->data;
+	if(right->data==target)return left->data;
+	int mirrorValue=findMirror(target,left->left,right->right);
+	if(mirrorValue)return mirrorValue;
+	findMirror(target,left->right,right->left);
+}
+int mirrorElement(tptr root,int target)
+{
+	if(root==NULL)return 0;
+	if(root->data==target)return target;
+	return findMirror(target,root->left,root->right);
+}
+void inorder(tptr root)
+{
+	if(root==NULL)return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+int main()
+{
+	int n,t,target;
+	cin>>n;
+	tptr root=NULL;
+	while(n!=-1)
+	{
+		root=bstInsert(root,n);
+		cin>>n;
+	}
+	inorder(root);
+	cin>>t;
+	while(t--)
+	{
+		cin>>target;	
+		cout<<mirrorElement(root,target)<<endl;
+	}
+	return 0;
+}
---
+++ All Concepts Systematically/TreesInC++/XORBinaryTreeConversion.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+struct Node{
+	int data;
+	struct Node *left,*right;
+	Node(int data)
+	{
+		this->data=data;
+		left=right=NULL;
+	}
+};
+typedef struct Node* tptr;
+void convertTree(tptr root)
+{
+	if(root==NULL)return;
+	convertTree(root->left);
+	convertTree(root->right);
+	if(root->left!=NULL && root->right!=NULL)
+		root->data = root->left->data | root->right->data;
+}
+void inorder(tptr root)
+{
+	if(root==NULL)return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+int main()
+{
+	tptr root=new Node(0);
+	root->left=new Node(1);
+	root->right=new Node(1);
+	root->left->left=new Node(0);
+	root->left->right=new Node(1);
+	root->right->left=new Node(1);
+	inorder(root);
+	puts("");
+	convertTree(root);
+	inorder(root);
+	return 0;
+}
