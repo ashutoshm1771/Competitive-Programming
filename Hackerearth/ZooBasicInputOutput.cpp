96d2e0cce1d3460a5b93c528d11a189110cf2ae3
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-04-17 00:07:45 +0530

    Create ZooBasicInputOutput.cpp

---
+++ Hackerearth/ZooBasicInputOutput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int zc=0,oc=0;
+    for(int i=0;s[i];i++)
+    {
+        if(s[i]=='z')zc++;
+        else oc++;
+    }
+    if(2*zc != oc ) cout<<"No";
+    else cout<<"Yes";
+    return 0;
+}
