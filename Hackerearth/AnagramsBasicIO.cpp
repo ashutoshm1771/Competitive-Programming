030be4b49f818f0950423bb8ebaa7024e3d57537
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-04-17 00:39:22 +0530

    Create AnagramsBasicIO.cpp

---
+++ Hackerearth/AnagramsBasicIO.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<set>
+#include<map>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    string al="abcdefghijklmnopqrstuvwxyz";
+    while(t--)
+    {
+        string a,b;
+        cin>>a>>b;
+        int c=0;
+        map<char,int> m;
+        for(int i=0;a[i];i++)
+        {
+            m[a[i]]++;
+            
+        }
+        for(int i=0;b[i];i++)
+        {
+            m[b[i]]--;
+            
+        }
+        for(int i=0;al[i];i++)
+        {
+            if(m[al[i]]<0)c-=m[al[i]];
+            else c+=m[al[i]];
+        }
+        cout<<c<<endl;
+    }
+    return 0;
+}
