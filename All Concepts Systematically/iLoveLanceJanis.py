78a967f54467e041524d69654005a0dea85bb443
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-05-13 16:59:37 +0530

    Update iLoveLanceJanis.py

--- All Concepts Systematically/iLoveLanceJanis.py
+++ All Concepts Systematically/iLoveLanceJanis.py
@@ -1,1 +1,8 @@
 def solution(x):
+    x=list(x)
+    for i in range(len(x)):
+        if(x[i].islower()):
+            x[i]=chr(122-(ord(x[i])-97))
+    return "".join(x)
+x=input()
+print(solution(x))
