61a3bcccbc99216ab87c336004ab2f7885b5d8b2
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-05-13 16:42:33 +0530

    FooBar

---
+++ All Concepts Systematically/BunnyPrisonerLocating.py
@@ -0,0 +1,114 @@
+def multiply(num1, num2): 
+	len1 = len(num1) 
+	len2 = len(num2) 
+	if len1 == 0 or len2 == 0: 
+		return "0" 
+	result = [0] * (len1 + len2) 
+	i_n1 = 0
+	i_n2 = 0 
+	for i in range(len1 - 1, -1, -1): 
+		carry = 0
+		n1 = ord(num1[i]) - 48
+		i_n2 = 0
+		for j in range(len2 - 1, -1, -1): 
+			n2 = ord(num2[j]) - 48
+			summ = n1 * n2 + result[i_n1 + i_n2] + carry  
+			carry = summ // 10
+			result[i_n1 + i_n2] = summ % 10
+			i_n2 += 1
+		if (carry > 0): 
+			result[i_n1 + i_n2] += carry 
+		i_n1 += 1 
+	i = len(result) - 1
+	while (i >= 0 and result[i] == 0): 
+		i -= 1 
+	if (i == -1): 
+		return "0"
+	s = "" 
+	while (i >= 0): 
+		s += chr(result[i] + 48) 
+		i -= 1
+	return s 
+def findSum(str1, str2):  
+    if (len(str1) > len(str2)): 
+        t = str1; 
+        str1 = str2; 
+        str2 = t; 
+    str = "";  
+    n1 = len(str1); 
+    n2 = len(str2);    
+    str1 = str1[::-1];  
+    str2 = str2[::-1];  
+    carry = 0;  
+    for i in range(n1):   
+        sum = ((ord(str1[i]) - 48) + 
+              ((ord(str2[i]) - 48) + carry));  
+        str += chr(sum % 10 + 48);    
+        carry = int(sum / 10);    
+    for i in range(n1, n2):  
+        sum = ((ord(str2[i]) - 48) + carry);  
+        str += chr(sum % 10 + 48);  
+        carry = (int)(sum / 10);  
+    if (carry):  
+        str += chr(carry + 48);  
+    str = str[::-1];  
+    return str; 
+def isSmaller(str1, str2): 
+    n1 = len(str1)  
+    n2 = len(str2) 
+    if (n1 < n2): 
+        return True
+    if (n2 < n1): 
+        return False
+    for i in range(n1): 
+        if (str1[i] < str2[i]): 
+            return True
+        elif (str1[i] > str2[i]): 
+            return False
+    return False
+def findDiff(str1, str2): 
+    if (isSmaller(str1, str2)): 
+        temp = str1 
+        str1 = str2 
+        str2 = temp  
+    str3 = "" 
+    n1 = len(str1)  
+    n2 = len(str2) 
+    str1= str1[::-1] 
+    str2 = str2[::-1]   
+    carry = 0
+    for i in range(n2): 
+        sub = ((ord(str1[i])-ord('0'))-(ord(str2[i])-ord('0'))-carry) 
+        if (sub < 0): 
+            sub = sub + 10
+            carry = 1
+        else: 
+            carry = 0
+        str3 = str3+str(sub ) 
+    for i in range(n2,n1): 
+        sub = ((ord(str1[i])-ord('0')) - carry)  
+        if (sub < 0): 
+            sub = sub + 10
+            carry = 1
+        else: 
+            carry = 0
+        str3 = str3+str(sub )  
+    str3= str3[::-1] 
+    return str3
+
+def sumUpto(x):
+    if(x&1):
+        a=int((x+1)/2)
+        b=x
+    else:
+        a=int(x/2)
+        b=x+1
+    return multiply(str(a),str(b))
+
+def solution(x,y):
+    s=findSum(str(1),sumUpto(x+y-1))
+    s=findDiff(s,str(y))
+    return s.lstrip("0")
+
+x,y=map(int,input().split())
+print(solution(x,y))
---
+++ All Concepts Systematically/GreatestMultipleOfThree.py
@@ -0,0 +1,59 @@
+MAX_SIZE = 10
+def sortCount(arr, n): 
+	count = [0]*MAX_SIZE 
+	for i in range(n): 
+		count[arr[i]] += 1
+	index = 0
+	for i in range(MAX_SIZE): 
+		while count[i] > 0: 
+			arr[index] = i 
+			index += 1
+			count[i] -= 1
+def removeAndResult(arr, n, ind1, ind2=-1): 
+	num=0
+	for i in range(n-1, -1, -1): 
+		if i != ind1 and i != ind2: 
+			num = num*10 + arr[i]
+	return num
+def solution(l):
+	n=len(l)
+	s = sum(l) 
+	if s % 3 == 0: 
+		num=0
+		l.sort(reverse=True)
+		for i in l:
+			num = num*10 + i
+		return num
+	sortCount(l, n) 
+	remainder = s % 3 
+	if remainder == 1: 
+		rem_2 = [0]*2
+		rem_2[0] = -1; rem_2[1] = -1
+		for i in range(n):  
+			if l[i] % 3 == 1: 
+				return removeAndResult(l, n, i) 
+			if l[i] % 3 == 2: 
+				if rem_2[0] == -1: 
+					rem_2[0] = i 
+				elif rem_2[1] == -1: 
+					rem_2[1] = i 
+		if rem_2[0] != -1 and rem_2[1] != -1: 
+			return removeAndResult(l, n, rem_2[0], rem_2[1]) 
+	elif remainder == 2: 
+		rem_1 = [0]*2
+		rem_1[0] = -1; rem_1[1] = -1 
+		for i in range(n): 
+			if l[i] % 3 == 2: 
+				return removeAndResult(l, n, i) 
+			if l[i] % 3 == 1: 
+				if rem_1[0] == -1: 
+					rem_1[0] = i 
+				elif rem_1[1] == -1: 
+					rem_1[1] = i 
+		if rem_1[0] != -1 and rem_1[1] != -1: 
+			return removeAndResult(l, n, rem_1[0], rem_1[1]) 
+	return 0
+if __name__ == "__main__": 
+	arr = [4, 4, 1, 1, 1, 3] 
+	n = len(arr) 
+	print(solution(arr, n))
