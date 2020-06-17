4ec3470253d2a120dc46b4102e20c66f722c2166
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-06-17 21:14:17 +0530

    CodeChef

---
+++ CodeChef/Demo.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#define ordered_set tree<long long int,null_type,less<long long int>,rb_tree_tag,tree_order_statistics_node_update>
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long int ll;
+int main()
+{
+    IOS
+    int t;
+    cin>>t;
+    cout<<t;
+    while(t--)
+    {
+        ll n,c=0,prev,m,j;
+        cin>>n;
+        cout<<n;
+        prev=n;
+        vector<ll> v;
+        for(ll i=0;i<n;i++)
+            cin>>v[i];
+        
+    }
+    return 0;
+}
---
+++ CodeChef/MinimumCards.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+/* //--
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> // oset.order_of_key(a); --> # of e < a
+using namespace __gnu_pbds;   // *(oset.find_by_order(a)); --> (a-1)th smallest e  
+#define orderedSet tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+//-- */  
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vi vector<int>
+#define pb push_back 
+#define fo(i,n) for(int i=0;i<n;i++)
+#define fo1(i,a,b) for(int i=a;i<=b;i++)
+#define fo2(i,a,b) for(int i=a;i<b;i++)
+#define fo3(i,s) for(int i=0;s[i];i++)
+#define fo4(x,s) for(auto x: s)
+#define all(v) v.begin(),v.end();
+using namespace std;
+typedef long long int ll;
+
+int minNum(string s)
+{
+    int i=0,ans=0;
+    bool fl=false;
+    // while(i<s.size())
+    // {
+    //     while(s[i]==s[i+1]){
+    //         i++;
+    //         ans++;
+    //         if(i==s.size()){
+    //             fl=true;
+    //             break;
+    //         }
+    //     }
+    //     if(fl)break;
+    //     i++;
+    // }
+    for(int i=0;s[i];i++)
+    {
+        if(s[i]==s[i+1])ans++;
+    }
+    return ans;
+}
+void solve()
+{
+    int n; cin>>n;
+    string s;
+    cin>>s;
+    cout<<minNum(s)<<endl;
+}
+int main()
+{
+    IOS
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
