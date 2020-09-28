0780a90da4a2e6aaa7ae408eb604a2785e9e4985
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-09-28 20:09:28 +0545

    Add files via upload

---
+++ Hackerearth/DefaultCodeProtocolCompetitive.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+/* //--
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> // oset.order_of_key(a); --> # of e < a
+using namespace __gnu_pbds;   // *(oset.find_by_order(a)); --> (a-1)th smallest e  
+#define orderedSet tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+//-- */  
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define vi vector<int>
+#define pb push_back 
+#define fo(i,n) for(int i=0;i<n;i++)
+#define fo1(i,a,b) for(int i=a;i<=b;i++)
+#define fo2(i,a,b) for(int i=a;i<b;i++)
+#define fo3(i,s) for(int i=0;s[i];i++)
+#define fo4(x,s) for(auto x: s)
+#define all(v) v.begin(),v.end()
+using namespace std;
+typedef long long int ll;
+
+void test_case()
+{
+      
+}
+signed main(void)
+{
+    IOS
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        test_case();
+    }
+    return 0;
+}
