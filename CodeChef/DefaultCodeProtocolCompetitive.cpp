05a2f5d3f9845478a506d5a304d796819dfbf5fc
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-09-28 19:53:53 +0530

    Update DefaultCodeProtocolCompetitive.cpp

--- CodeChef/DefaultCodeProtocolCompetitive.cpp
+++ CodeChef/DefaultCodeProtocolCompetitive.cpp
@@ -6,6 +6,7 @@ using namespace __gnu_pbds;   // *(oset.find_by_order(a)); --> (a-1)th smallest 
 #define orderedSet tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
 //-- */  
 #define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 #define vi vector<int>
 #define pb push_back 
 #define fo(i,n) for(int i=0;i<n;i++)
@@ -13,22 +14,22 @@ using namespace __gnu_pbds;   // *(oset.find_by_order(a)); --> (a-1)th smallest 
 #define fo2(i,a,b) for(int i=a;i<b;i++)
 #define fo3(i,s) for(int i=0;s[i];i++)
 #define fo4(x,s) for(auto x: s)
-#define all(v) v.begin(),v.end();
+#define all(v) v.begin(),v.end()
 using namespace std;
 typedef long long int ll;
 
-void solve()
+void test_case()
 {
-    
+      
 }
-int main()
+signed main(void)
 {
     IOS
-    int t;
+    int t=1;
     cin>>t;
     while(t--)
     {
-        solve();
+        test_case();
     }
     return 0;
 }
