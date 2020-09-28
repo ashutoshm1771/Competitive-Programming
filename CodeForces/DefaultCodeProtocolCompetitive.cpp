4579a56f42527d14b5777167859a768c2ebc257c
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-09-28 19:54:09 +0530

    Update DefaultCodeProtocolCompetitive.cpp

--- CodeForces/DefaultCodeProtocolCompetitive.cpp
+++ CodeForces/DefaultCodeProtocolCompetitive.cpp
@@ -1,41 +1,35 @@
-// Author : Ashutosh Mishra
-
 #include<bits/stdc++.h>
-
 /* //--
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp> // oset.order_of_key(a); --> # of e < a
 using namespace __gnu_pbds;   // *(oset.find_by_order(a)); --> (a-1)th smallest e  
 #define orderedSet tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
 //-- */  
-
 #define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 #define vi vector<int>
-#define vll vector<long long int>
 #define pb push_back 
 #define fo(i,n) for(int i=0;i<n;i++)
 #define fo1(i,a,b) for(int i=a;i<=b;i++)
 #define fo2(i,a,b) for(int i=a;i<b;i++)
 #define fo3(i,s) for(int i=0;s[i];i++)
 #define fo4(x,s) for(auto x: s)
-#define all(v) v.begin(),v.end();
-
+#define all(v) v.begin(),v.end()
 using namespace std;
 typedef long long int ll;
 
-void solve()
+void test_case()
 {
-    
+      
 }
-
-int main()
+signed main(void)
 {
     IOS
     int t=1;
     cin>>t;
     while(t--)
     {
-        solve();
+        test_case();
     }
     return 0;
 }
