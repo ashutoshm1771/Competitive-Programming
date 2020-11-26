c36d67286bc8764f0aebe0175c1b79de2869fe21
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-11-26 19:22:49 +0530

    Update Scanner.java

--- other_algorithms/Scanner.java
+++ other_algorithms/Scanner.java
@@ -11,6 +11,7 @@ import java.util.StringTokenizer;
 class Scanner 
 {
 	StringTokenizer st;
+
 	BufferedReader br;
 
 	public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
